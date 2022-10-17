adeaa17221dd14d98ca11534e7cab68149672793
Tobi Samuel <ufomatobi@gmail.com>
2022-10-17 09:22:11 +0100

    fix allowed origins array

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
-  "https://the-noted-app.netlify.app",
+  "https://agrishop.vercel.app",
   "http://127.0.0.1:5173",
-  "http://127.0.0.1:3000",
   "http://localhost:5173",
+  "http://127.0.0.1:3000",
   "http://localhost:3000",
 ];
 
