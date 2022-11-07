25e94065d157478af70211630c499bc9eef8ae4d
Tobi Samuel <ufomatobi@gmail.com>
2022-11-07 22:11:57 +0100

    add new origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
   "https://agrishop.vercel.app",
+  "https://lambent-rabanadas-d7be42.netlify.app",
   "http://127.0.0.1:5173",
   "http://localhost:5173",
   "http://127.0.0.1:3000",
