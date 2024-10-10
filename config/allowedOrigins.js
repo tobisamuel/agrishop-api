234fc8895953edeabb743713e00eb66ff25015ff
Tobi Samuel <ufomatobi@gmail.com>
2024-10-10 09:25:47 +0100

    fix: add url to allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "https://agrishop.vercel.app",
   "https://lambent-rabanadas-d7be42.netlify.app",
+  "https://bespoke-jalebi-e586fb.netlify.app",
   "http://127.0.0.1:5173",
   "http://localhost:5173",
   "http://127.0.0.1:3000",
